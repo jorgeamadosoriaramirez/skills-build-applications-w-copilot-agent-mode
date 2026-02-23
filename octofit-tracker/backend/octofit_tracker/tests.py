09f639125e464ce3dd66cad4720be6d53ac19785
jorgeamadosoriaramirez <jorgeamadosoria@gmail.com>
2026-02-23 18:17:12 +0000

    Update Django app for MongoDB, CORS, and all collections

---
+++ octofit-tracker/backend/octofit_tracker/tests.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+from .models import User, Team, Activity, Leaderboard, Workout
+
+class UserModelTest(TestCase):
+    def test_create_user(self):
+        user = User.objects.create(email='test@example.com', name='Test User', team='Marvel', superpower='Flying')
+        self.assertEqual(user.email, 'test@example.com')
+
+class TeamModelTest(TestCase):
+    def test_create_team(self):
+        team = Team.objects.create(name='Marvel')
+        self.assertEqual(team.name, 'Marvel')
+
+class ActivityModelTest(TestCase):
+    def test_create_activity(self):
+        user = User.objects.create(email='test2@example.com', name='Test User2', team='DC', superpower='Strength')
+        activity = Activity.objects.create(user=user, type='Running', duration=30, date='2026-02-23')
+        self.assertEqual(activity.type, 'Running')
+
+class LeaderboardModelTest(TestCase):
+    def test_create_leaderboard(self):
+        leaderboard = Leaderboard.objects.create(team='Marvel', points=100)
+        self.assertEqual(leaderboard.team, 'Marvel')
+
+class WorkoutModelTest(TestCase):
+    def test_create_workout(self):
+        workout = Workout.objects.create(name='Pushups', description='Do 20 pushups', suggested_for='Marvel')
+        self.assertEqual(workout.name, 'Pushups')
